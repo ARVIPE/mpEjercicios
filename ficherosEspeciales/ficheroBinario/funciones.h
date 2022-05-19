0338cb45d57e07b84c71186707a30846343d52ff
ARVIPE <arvipe@hotmail.com>
2022-05-19 18:08:41 +0200

    ejercicios binarios en progreso

---
+++ ficherosEspeciales/ficheroBinario/funciones.h
@@ -0,0 +1,11 @@
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+
+struct proceso{
+    char nombre[15];
+    int duracion;
+    int prioridad;
+};
+
+
+#endif
---
+++ ficherosEspeciales/ficheroBinario/main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include "funciones.h"
+
+int main(int argc, char ** argv[]){
+    if(argc!=4){
+        printf("ERROR: faltan argumentos\n");
+        exit(-1);
+    }
+}
